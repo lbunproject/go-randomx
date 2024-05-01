3f70ec75be4f8f6434cd7ce810acc3791734c9b4
WeebDataHoarder <57538841+WeebDataHoarder@users.noreply.github.com>
2024-05-01 07:36:26 +0200

    Remove unused functions on cpuid_amd64

--- asm/cpuid_amd64.go
+++ asm/cpuid_amd64.go
@@ -3,5 +3,3 @@
 package asm
 
 func Cpuid(op uint32) (eax, ebx, ecx, edx uint32)
-func Cpuidex(op, op2 uint32) (eax, ebx, ecx, edx uint32)
-func Xgetbv(index uint32) (eax, edx uint32)
--- asm/cpuid_amd64.s
+++ asm/cpuid_amd64.s
@@ -13,22 +13,3 @@ TEXT ·Cpuid(SB), 7, $0
         MOVL DX, edx+20(FP)
         RET
 
-
-// func Cpuidex(op, op2 uint32) (eax, ebx, ecx, edx uint32)
-TEXT ·Cpuidex(SB), 7, $0
-        MOVL op+0(FP), AX
-        MOVL op2+4(FP), CX
-        CPUID
-        MOVL AX, eax+8(FP)
-        MOVL BX, ebx+12(FP)
-        MOVL CX, ecx+16(FP)
-        MOVL DX, edx+20(FP)
-        RET
-
-// func xgetbv(index uint32) (eax, edx uint32)
-TEXT ·Xgetbv(SB), 7, $0
-        MOVL index+0(FP), CX
-        BYTE $0x0f; BYTE $0x01; BYTE $0xd0 // XGETBV
-        MOVL AX, eax+8(FP)
-        MOVL DX, edx+12(FP)
-        RET
